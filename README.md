e42bc30ba388309c838e6743ca6c0b30be32f49c
Анастасия <ger.anastasia.a@gmail.com>
2022-11-08 17:03:49 +0200

    Lesson_04 Generators

--- README.md
+++ README.md
@@ -25,3 +25,14 @@ pipenv lock
 # Pipenv lock & pipenv sync
 pipenv update
 ```
+
+## Information for lesson 4
+
+### Download rockyou file for work
+We need rockyou file for creating filtering function. You can [download](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwisgfT2-ZT4AhVJxIsKHR9wB4IQFnoECAgQAQ&url=https%3A%2F%2Fgithub.com%2Fbrannondorsey%2Fnaive-hashcat%2Freleases%2Fdownload%2Fdata%2Frockyou.txt&usg=AOvVaw3snAERl1mU6Ccr4WFEazBd) this file.
+
+### Install components for getting size of the files
+```bash
+# install pympler
+pip install pympler
+```
---
+++ lesson_04/4.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import Generator
+
+from pympler import asizeof
+
+LESSON_04_DIR = Path(__file__).parent
+ROCKYOU_FILENAME = LESSON_04_DIR / "rockyou.txt"
+FILTERED_DATA_FILE = LESSON_04_DIR / "filtered_data.txt"
+
+johns = []
+
+print(f'Size of file "rockyou" before filter - {asizeof.asizeof(ROCKYOU_FILENAME)}')
+
+
+def filter_lines(filename: Path, pattern: str) -> Generator:
+    with open(filename, encoding="utf-8") as file:
+        while True:
+            line = file.readline().replace("\n", "")
+
+            if not line:
+                break
+
+            if pattern in line.lower():
+                yield line
+
+
+filtered_names = filter_lines(ROCKYOU_FILENAME, "john")
+
+
+def save_filtered_lines(filename: Path, data: Generator):
+    with open(filename, "w") as file:
+        file.writelines("%s\n" % line for line in data)
+
+
+save_filtered_lines(FILTERED_DATA_FILE, filtered_names)
+line_count = sum(1 for line in open(FILTERED_DATA_FILE))
+line_count_rockyou = sum(1 for line1 in open(ROCKYOU_FILENAME))
+
+print(f'Total lines in file "rockyou" -  {line_count_rockyou}')
+print(f"Total lines in file with filtered names - {line_count}")
+
+print(f'Size of file "rockyou" - {asizeof.asizeof(ROCKYOU_FILENAME)}')
+print(f"Size of file with filtered names - {asizeof.asizeof(FILTERED_DATA_FILE)}")
